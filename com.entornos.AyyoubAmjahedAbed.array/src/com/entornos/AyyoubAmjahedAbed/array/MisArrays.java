f2e4c1030492d68f0dcfbae0e211c57fecb9f2a4
Ayyoub Amjahed <ayyub2000@gmail.com>
2023-04-17 10:57:07 +0200

    Proyecto arrays ahora con documentacion

--- com.entornos.AyyoubAmjahedAbed.array/src/com/entornos/AyyoubAmjahedAbed/array/MisArrays.java
+++ com.entornos.AyyoubAmjahedAbed.array/src/com/entornos/AyyoubAmjahedAbed/array/MisArrays.java
@@ -1,10 +1,19 @@
 package com.entornos.AyyoubAmjahedAbed.array;
 
+/**
+ * 
+ * Se importan las herramientas para trabajr con arrays
+ */
 import java.util.Arrays;
 
 
 public abstract class MisArrays {
-    
+    /**
+     * 
+     * Se crea el metodo media que calcula la media
+     * @param numeros
+     * @return
+     */
     public static double media(int[] numeros) {
         int suma = 0;
         for (int i = 0; i < numeros.length; i++) {
@@ -12,6 +21,12 @@ public abstract class MisArrays {
         }
         return (double) suma / numeros.length;
     }
+    
+    /**
+     * Se crea el metodo mediana que calcula la mediana
+     * @param numeros
+     * @return
+     */
     
     public static double mediana(int[] numeros) {
         Arrays.sort(numeros);
@@ -23,6 +38,11 @@ public abstract class MisArrays {
         }
     }
     
+    /**
+     * Se crea el metodo maximo que devuelve el valor maximo 
+     * @param numeros
+     * @return
+     */
     public static int maximo(int[] numeros) {
         int max = numeros[0];
         for (int i = 1; i < numeros.length; i++) {
@@ -32,6 +52,12 @@ public abstract class MisArrays {
         }
         return max;
     }
+    
+    /**
+     * Se crea el metodo minimo que devuelve el valor minimo
+     * @param numeros
+     * @return
+     */
     
     public static int minimo(int[] numeros) {
         int min = numeros[0];
